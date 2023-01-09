9b58a71110f837b818829d1de8fd8cb4f67cb6d4
rayanshahin <rayanahmad3011@gmail.com>
2023-01-09 17:43:09 +0300

    this is the screenshot folder

--- src/myclass.java
+++ src/myclass.java
@@ -1,6 +1,12 @@
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Date;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,32 +15,58 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class myclass {
 
-	public static void main(String[] args) {
-		WebDriver driver;
+	public static void main(String[] args) throws IOException {
 		
 
 		WebDriverManager.chromedriver().setup();
+		WebDriver driver;
+		driver = new ChromeDriver();
 
-			driver = new ChromeDriver();
-			driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));//to wait 3 secounds 
 
-			driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/button")).click();
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/button")).click();
 
-			driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/button[1]")).click();
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/button[1]")).click();
 
-			String[] myListOfFirstName = { "ahmad", "ali", "marwa", "anas", };
+		String[] myListOfFirstName = { "ahmad", "ali", "marwa", "anas", };
 
-			int randomOne = (int) (Math.random() * myListOfFirstName.length - 1);
+		int randomOne = (int) (Math.random() * myListOfFirstName.length - 1);
 
-			String[] myListOfLastName = { "omar", "mahmoud", "eman", "enas", };
+		String[] myListOfLastName = { "omar", "mahmoud", "eman", "enas", };
 
-			int randomTwo = (int) (Math.random() * myListOfLastName.length - 1);
+		int randomTwo = (int) (Math.random() * myListOfLastName.length - 1);
 
-			WebElement myInputForTheFirstName = driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[1]/input"));
-			myInputForTheFirstName.sendKeys(myListOfFirstName[randomOne]);
+		WebElement myInputForTheFirstName = driver
+				.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[1]/input"));
+		myInputForTheFirstName.sendKeys(myListOfFirstName[randomOne]);
 
+		WebElement myInputForTheLastName = driver
+				.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[2]/input"));
+
+		myInputForTheLastName.sendKeys(myListOfLastName[randomTwo]);
+
+		int PostalCodeRandomNumber = (int) (Math.random() * 10000);
+
+		String PostalCodeString = Integer.toString(PostalCodeRandomNumber);
+		driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[3]/input"))
+				.sendKeys(PostalCodeString);
+		Date currentTime = new Date();
+
+		String myUpdatedate = currentTime.toString();
+
+		String NewDate = myUpdatedate.replace(":", "-");
+
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File file = ts.getScreenshotAs(OutputType.FILE);
+
+		FileUtils.copyFile(file, new File("./ScreenShot_Folder/" + NewDate + ".jpg"));
+
+		/*
+		 * Date mydateeeeee = new Date(); //Date timestamp = new Date();
+		 * System.out.println(mydateeeeee);
+		 */
 	}
 
 }
